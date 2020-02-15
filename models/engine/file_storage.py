d18687593fe101a9473ede188e1bf4701519f409
Michelle Giraldo <michellegsld@gmail.com>
2020-02-15 01:11:45 -0500

    [Update] Fix save() and reload() to follow comments, insert comments, make pep8 compliant

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python3
-"""handling JSON files """
+"""
+file_storage.py
+FileStorage Class
+"""
 import json
 import os.path
 
 
-class FileStorage():
-    __file_path = file.json
+class FileStorage:
+    """
+    Serializes instances to a JSON file as storage
+    Deserializes JSON file to instances to recreate saved instances
+    Contains the class attributes:
+    - __file_path -> String. Path to JSON file.
+    - __objects -> Dictionary. To contain objects.
+    """
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """
+        Returns the __objects dictionary
+        """
         return type(self).__objects
 
     def new(self, obj):
-        key = str(type(obj).__name) + '.' + str(obj.id)
+        """
+        Populates the __objects dictionary
+        Uses this format for creating the keys of each value:
+        <class name of object>.<id of object>
+        """
+        key = str(type(obj).__name__) + '.' + str(obj.id)
         type(self).__objects[key] = obj
 
     def save(self):
+        """
+        Serializes the dictionary __objects to the JSON file
+        To do so, a copy of __objects must be made in order to:
+        -> save dictionary of attributes of object/instance within copy
+        """
         temp = {}
         temp.update(type(self).__objects)
         for k, v in temp.items():
@@ -24,6 +47,14 @@ class FileStorage():
             json.dump(temp, fil)
 
     def reload(self):
-        if os.path.isfile((type)self.__file_path):
+        """
+        Deserializes the JSON file back into __objects
+        To do so, it has to re-create the object using:
+        -> dictionarys of attributes saved as the values within the dict
+        """
+        if os.path.isfile(type(self).__file_path):
+            from models.base_model import BaseModel
             with open(self.__file_path) as fil:
-                json.load(fil)
+                dic = json.load(fil)
+                for k, v in dic.items():
+                    FileStorage.__objects[k] = BaseModel(**v)
