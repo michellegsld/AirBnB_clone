79f9537b557a62e03b333c644fbb57f8bb5af938
Michelle Giraldo <michellegsld@gmail.com>
2020-02-18 16:05:03 -0500

    [Fix] Misplaced test_file_storage.py, create package

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Unittest for file_storage.py
+"""
+import unittest
+from models.engine.file_storage import FileStorage
+import os.path
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    Contains multiple tests
+    """
+
+    def test_save_to_file(self):
+        """
+        Tests if an instance was saved to a file
+        """
+        pass
