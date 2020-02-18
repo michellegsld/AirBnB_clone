f63ca8543a560fa6bf725f7d08c89e2d91079e35
Michelle Giraldo <michellegsld@gmail.com>
2020-02-18 15:26:58 -0500

    [Fix] Add shebang and comments

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+This file is needed to create a python package
+It creates the variable storage which is used to save instances
+"""
 from models.engine import file_storage
 
 storage = file_storage.FileStorage()
